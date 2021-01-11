169e09a8c89a2d36d3139a488dcb743953bef446
Aleksandr Karakchiev <realrushen@gmail.com>
2021-01-11 22:28:08 +0300

    linter error fixed, README.md created

--- app/storage/mixins.py
+++ app/storage/mixins.py
@@ -2,8 +2,8 @@ from rest_framework import mixins
 
 
 class CreateRetrieveDestroyModelMixin(mixins.CreateModelMixin,
-                    mixins.RetrieveModelMixin,
-                    mixins.DestroyModelMixin):
+                                      mixins.RetrieveModelMixin,
+                                      mixins.DestroyModelMixin):
     """
     Custom mixin for Creating, Retrieving and Destroying instances
     """
